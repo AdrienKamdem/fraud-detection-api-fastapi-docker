30e1aadb2c7bcffec0e5d00b82556c94dec5b286
adrien kamdem <adrienkamdem@unknown.home>
2025-08-24 21:33:26 +0100

    added new files for pydantic data model checks

---
+++ fraud-detection/src/processor.py
@@ -0,0 +1,13 @@
+import csv
+import os
+import logging
+import sys
+import pydantic
+
+class Processor():
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    print("Hello World")
