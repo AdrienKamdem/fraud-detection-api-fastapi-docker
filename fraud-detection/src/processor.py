5997f307b0c8e069c57689d0843fdc5c555d59a2
adrien kamdem <adrienkamdem@unknown.home>
2025-08-24 22:00:08 +0100

    couple changes on the processor side

--- fraud-detection/src/processor.py
+++ fraud-detection/src/processor.py
@@ -1,13 +1,33 @@
 import csv
 import os
+import pandas as pd
 import logging
 import sys
-import pydantic
+
+
+logger = logging.getLogger()
+logger.setLevel(level=logging.DEBUG)
 
 class Processor():
-    def __init__(self):
+    
+    def __init__(self, path:str):
+        self.path = path
+    
+    def __str__(self):
+        pass
+    
+    def data_analysis(self):
+        df = pd.read_csv(self.path)
+        print("Dataset NaN stats")
+        print(df.isna().sum())
+        print("Dataset Summary stats")
+        print(df.dtypes)
+    
+    def processing_data(self):
         pass
 
-
 if __name__ == "__main__":
-    print("Hello World")
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    path = os.path.join(BASE_DIR, "data", "synthetic_fraud_dataset.csv")
+    dataset = Processor(path=path)
+    dataset.data_analysis()
