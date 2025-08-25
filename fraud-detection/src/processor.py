e2357f67acdb398f95415eb1e85eea25a291e77c
adrien kamdem <adrienkamdem@mac.home>
2025-08-25 10:00:21 +0100

    adding analysis on the dataset

--- fraud-detection/src/processor.py
+++ fraud-detection/src/processor.py
@@ -17,11 +17,34 @@ class Processor():
         pass
     
     def data_analysis(self):
+
         df = pd.read_csv(self.path)
+
+        print("\n\n*******")
         print("Dataset NaN stats")
         print(df.isna().sum())
+
+        print("\n\n*******")
         print("Dataset Summary stats")
         print(df.dtypes)
+        data_type_f = pd.DataFrame()
+        for col in df.columns:
+            if df.dtypes[col] == "object":
+                data_type_f[col] = df[col]
+        
+        print("\n\n*******")
+        print("Dataset to be formatted")
+        print(data_type_f.head(5))
+
+        print("\n\n*******")
+        print("Unique values per columns")
+        for col in data_type_f.columns:
+            print(f"For column: {col}")
+            print(data_type_f[col].unique())
+            print(f"Entire number of row : {data_type_f.shape[0]}\nNumber of unique values: {len(data_type_f[col].unique())}")
+            print("\n\n")
+
+
     
     def processing_data(self):
         pass
